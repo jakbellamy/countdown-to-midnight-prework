e5fe41d7cedcc5b13d22c8bed29c326fc0f12c2d
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-03 20:07:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(number)
-  countdown_with_sleep number
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
+  countdown_with_sleep number
   "HAPPY NEW YEAR!"
 end
